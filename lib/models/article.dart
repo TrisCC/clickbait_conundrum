d482cb0652f7d4bde4f633a03a25e0c2712d9a3a
Tristan <tristancotino@gmail.com>
2023-05-10 14:44:16 +0200

    Add basic packages and article model

---
+++ lib/models/article.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+class Article {
+  final int id;
+  final String title;
+  final String? text;
+  final String source;
+  final String url;
+  final String? imageUrl;
+
+  Article(this.id, this.title, this.text, this.source, this.url, this.imageUrl);
+
+  static List<Article> csvToArticleList(String data) {
+    List<Article> articles = [];
+
+    List<String> articleStrings = data.split('\n');
+
+    for (int i = 1; i < articleStrings.length; i++) {
+      List<String> article = articleStrings[i].split(',');
+
+      // TODO: Look at data format
+      // articles.add(new Article(user[0].trim(), user[1].trim(), user[2].trim()));
+    }
+
+    return articles;
+  }
+}
