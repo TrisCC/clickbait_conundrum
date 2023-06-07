9acc7ded8c13edb8d00e92495d05565ffaea3943
TrisCC <tristancotino@gmail.com>
2023-06-07 14:32:59 +0200

    stuff

--- lib/data/article_repository.dart
+++ lib/data/article_repository.dart
@@ -1,5 +1,15 @@
 import 'package:csv/csv.dart';
+import 'package:flutter/services.dart';
+
+import '../models/models.dart';
 
 class ArticleRepository {
+  Future<List<Article>> getArticles() async {
+    final rawData = await rootBundle.loadString('assets/WELFake_Dataset.csv');
 
+    List<Article> articles = Article.csvToArticleList(rawData);
+
+
+    return [articles.first];
+  }
 }
