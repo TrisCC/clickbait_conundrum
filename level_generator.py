b8f848a9c47922221618ad34f3517bcaa3de7db3
TrisCC <tristancotino@gmail.com>
2023-06-28 19:53:44 +0200

    Fix generator script and generate levels with seed 1337

--- level_generator.py
+++ level_generator.py
@@ -3,6 +3,8 @@ import csv
 import random
 import sys
 
+random.seed(1337)
+
 def main():
     csv.field_size_limit(100000000)
     
@@ -27,20 +29,23 @@ def main():
         
         print(f"Real articles: {len(real_articles)}")
         print(f"Fake articles: {len(fake_articles)}")
+        
+        random.shuffle(fake_articles)
+        random.shuffle(real_articles)
 
         for fake_factor in range(10, 100, 10):
             print(f"Genrating levels for factor {fake_factor}")
-            for level_number in range (1, 12):
+            for level_number in range (1, 13):
                 print(f"Genrating level {level_number}")
                 # Generate level list
                 current_level = []
                 
                 for i in range(int(20 * (float(fake_factor) / 100))):
-                    current_level.append(fake_articles[1])
-                    fake_articles.pop(1)
-                for i in range(int(20 * (100 - float(fake_factor) / 100))):
-                    current_level.append(real_articles[1])
-                    real_articles.pop(1)
+                    current_level.append(fake_articles[0])
+                    fake_articles.pop(0)
+                for i in range(int(20 * ((100 - float(fake_factor)) / 100))):
+                    current_level.append(real_articles[0])
+                    real_articles.pop(0)
                 
                 # Shuffle level
                 random.shuffle(current_level)
